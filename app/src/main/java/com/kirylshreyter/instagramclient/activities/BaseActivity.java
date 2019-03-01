0cf65166f6d59fde997e9f21a500d8326c0b81bb
Kiryl Shreyter <k.shreyter@unlitsolutions.com>
2019-03-01 15:17:31 +0300

    add logger to BaseActivity onCreate (to emulate git conflicts)

--- app/src/main/java/com/kirylshreyter/instagramclient/activities/BaseActivity.java
+++ app/src/main/java/com/kirylshreyter/instagramclient/activities/BaseActivity.java
@@ -2,6 +2,7 @@ package com.kirylshreyter.instagramclient.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class BaseActivity extends AppCompatActivity {
     private int layoutId;
@@ -12,6 +13,7 @@ public class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("ON_CREATE", "onCreate() called with: savedInstanceState = [" + savedInstanceState + "]");
         super.onCreate(savedInstanceState);
         setContentView(layoutId);
     }
